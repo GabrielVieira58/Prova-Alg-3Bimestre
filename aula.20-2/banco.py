d675f55b638f00980eba3e9a7d22f1cd0bc1959b
Gabriel Vieira <gabrielvieiradealmeida816@gmail.com>
2026-09-21 20:37:52 -0300

    Add files via upload

--- aula.20-2/banco.py
+++ aula.20-2/banco.py
@@ -1,5 +1,16 @@
 import json
 import datetime as dt
+import random
+
+
+ #21/09
+# print(hoje)
+# mes_atual = dt.datetime.month #9
+# proximo_mes = dt.datetime.__add__3
+# print(proximo_mes)
+
+# if hoje == mes_atual:
+#     proximo_mes = hoje.month + 1
 
 
 #Carregar ou criar uma conta
@@ -27,8 +38,10 @@ def depositar(conta, valor):
     if valor <= 0:
         print("O valor do depósito deve ser positivo.")
         return
+    dia = dt.datetime.day
+    now = dt.datetime.today()
     conta["saldo"] += valor
-    conta["historico"].append(f"Depósito de R$ {valor:.2f}")
+    conta["historico"].append(f"Depósito de R$ {valor:.2f}"), conta["historico"].append(f"Data do depósito: {now}")
     print(f"Depósito realizado com sucesso! Saldo Atual: R$ {conta['saldo']:.2f}")
 
     
@@ -40,8 +53,9 @@ def sacar(conta, valor):
     if valor > conta["saldo"]:
         print("Saldo insuficiente para saque.")
         return
+    now = dt.datetime.today()
     conta["saldo"] -= valor
-    conta["historico"].append(f"Saque de R$ {valor:.2f}")
+    conta["historico"].append(f"Saque de R$ {valor:.2f}"), conta["historico"].append(f"Data do saque: {now}")
     print(f"Saque realizado com sucesso! Saldo Atual: R$ {conta['saldo']:.2f}")
 
 
@@ -58,23 +72,59 @@ def extrato(conta):
 #Banco funcionando(Pybank)-------------------------------------------------------------------
 
 print("--- Bem-Vindo ao Pybank ---")
-conta = carregar_conta()
 
+
+#Aqui foi uma tentativa de fazer um sistema melhor, deixando a opção de criar contas, no entanto não consegui completar, queria que fosse 
+# possivel criar várias contas e acessar essas contas com o nome e senha corretos
+
+# usuarios = []
+
+# print("[1] Criar conta | [2] Entrar | [3] Sair")
+# op = input("Escolha: ")
+
+# if op == '1':
+#     usuario = {}
+#     usuario['nome_input'] = input("Conta não encontrada. Qual seu nome?\nR:  ")
+#     usuario['senha_input'] = int(input("Digite uma senha com 4 dígitos: "))
+#     usuario['saque_max'] = 1000
+#     usuario['limite_saque_atual'] = ''
+#     usuario['conta'] = {
+#         "nome": usuario[nome_input],
+#         "saldo" : 0.0,
+#         "Saque Máximo" : usuario[saque_max],
+#         "Limite Saque Atual" : usuario[limite_saque_atual],
+#         "senha" : usuario[senha_input],
+#         "historico" : [{
+#         }]
+#     }
+#-----------------------------------------------------------------------------------------------------------
+
+conta = carregar_conta()
 if conta is None:
     nome_input = input("Conta não encontrada. Qual seu nome?\nR:  ")
     senha_input = int(input("Digite uma senha com 4 dígitos: "))
+    saque_max = 1000
+    limite_saque_atual = ''
     conta = {
-        "nome": nome_input,
-        "saldo" : 0.0,
-        "senha" :  [],
+       "nome": nome_input,
+       "saldo" : 0.0,
+      "Saque Máximo" : saque_max,
+        "Limite Saque Atual" : limite_saque_atual,
+        "senha" : senha_input,
         "historico" : [{
         }]
     }
 else:
     print(f"Bem-vindo de volta, {conta['nome']}!")
+    senha_input = conta['senha']
+    saque_max = 1000
+    limite_saque_atual = ''
+    
 
 while True:
     print(f"O que deseja fazer?\nSaldo atual: R${conta['saldo']:.2f}")
+    print(saque_max)
+    print(limite_saque_atual)
     opcao = input("[1] Depositar | [2] Sacar | [3] Extrato | [4] Guardar e Sair: ")
     if opcao == "1" or opcao == "2":
         try:
@@ -83,12 +133,41 @@ while True:
             print("Digite um número válido.")
             continue
         if opcao == "1":
-            depositar(conta, valor)
+            senha_confir = int(input("Digite sua senha para realizar o depósito: "))
+            if senha_confir == senha_input:
+                depositar(conta, valor)
+                senha_confir = 0
 
-        else: sacar(conta, valor)
+            
+
+            else:
+                print("Sua senha está incorreta! Tente novamente!")
+                senha_confir = 0
+
+        elif opcao == "2" and saque_max > 0 and valor <= saque_max:
+            senha_confir = int(input("Digite sua senha para realizar o saque: "))
+            if senha_confir == senha_input:
+                    sacar(conta, valor)
+                    saque_atual = saque_max - valor
+                    saque_max = saque_atual
+
+                    senha_confir = 0
+
+            if valor > saque_max:
+                print("Seu limite diário de saque já foi excedido!")
+            
+
 
     elif opcao == "3":
-        extrato(conta)
+        senha_confir = int(input("Digite sua senha para ver seu extrato: "))
+        if senha_confir == senha_input:
+
+            senha_confir = 0
+            extrato(conta)
+        else:
+            print("Sua senha está incorreta! Tente novamente!")
+            senha_confir = 0
+
 
     elif opcao == "4":
         salvar_conta(conta)
